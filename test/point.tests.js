663a27765da7cc68c167cb2ee07992844bd5034d
Johan Jordaan <djjordaan@gmail.com>
2013-04-13 03:49:44 -0700

    Added the point utils

---
+++ test/point.tests.js
@@ -0,0 +1,34 @@
+var assert = require('assert')
+
+var point = require('../utils/point.js')
+
+var assert_point_values = function(point,x,y) {
+    assert.equal(point.x,x);
+    assert.equal(point.y,y);
+}
+
+
+describe('Point2D', function() {
+    describe('#constructor', function() {
+        it('should create a new point with all values set to the specified values', function() {
+            var p = new point.Point2D(10,10);
+            assert_point_values(p, 10, 10 );
+        })
+    })
+    describe('#set', function() {
+        it('should set the points coordinates to the new values', function() {
+            var p = new point.Point2D(10,10);
+            p.set(20,20);
+            assert_point_values(p, 20, 20 );
+        })
+    })
+    describe('#translate', function() {
+        it('should translate the points coordinates by the given delta values', function() {
+            var p = new point.Point2D(10,10);
+            p.translate(-20,20);
+            assert_point_values(p, -10, 30 );
+        })
+    })
+
+    
+})
---
+++ utils/point.js
@@ -0,0 +1,17 @@
+var Point2D = function(x,y) {
+    this.set(x,y);
+}
+Point2D.prototype.set = function(x,y) {
+    this.x = x;
+    this.y = y;
+}
+Point2D.prototype.translate = function(dx,dy) {
+    this.set(this.x+dx, this.y+dy); 
+}
+
+
+if(typeof module != 'undefined') {
+    module.exports.Point2D = Point2D;
+} else {
+    alert('point.js loaded');
+}
