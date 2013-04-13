4becbba5b6d843c1539a27e68541924c18c397e9
Johan Jordaan <djjordaan@gmail.com>
2013-04-13 04:45:46 -0700

    Updated vector to use points

--- test/vector.tests.js
+++ test/vector.tests.js
@@ -1,15 +1,68 @@
 var assert = require('assert')
 
-var vector = require('../utils/vector.js')
+var Point2D = require('../utils/point.js').Point2D;
+var Vector2D = require('../utils/vector.js').Vector2D;
+
+
+var assert_vactor_values = function(vector,x1,y1,x2,y2,dx,dy,d) {
+    assert.equal(vector.p1.x,x1);
+    assert.equal(vector.p1.y,y1);
+    assert.equal(vector.p2.x,x2);
+    assert.equal(vector.p2.y,y2);
+    assert.equal(vector.dx,dx);
+    assert.equal(vector.dy,dy);
+    assert.equal(vector.d,d);
+
+}
 
 describe('Vector2D', function() {
     describe('#constructor', function() {
         it('should create a new vector with all values set to the specified values', function() {
-            var v = new vector.Vector2D(0,0,10,10);
-            assert.equal(v.x1,0);
-            assert.equal(v.y1,0);
-            assert.equal(v.x2,10);
-            assert.equal(v.y2,10);
+            var v = new Vector2D(new Point2D(0,0),new Point2D(10,10));
+            assert_vactor_values(v, 0,0, 10,10, 10,10, Math.sqrt(10*10 + 10*10) );
+        })
+    })
+    
+    describe('#set', function() {
+        it('should set the values of of both points', function() {
+            var v = new Vector2D(new Point2D(0,0),new Point2D(10,10));
+            v.set(new Point2D(2,2),new Point2D(10,10))
+            assert_vactor_values(v, 2,2, 10,10, 8,8, Math.sqrt(8*8 + 8*8) );
+        })
+    })
+    describe('#set_p1', function() {
+        it('should set the values point 1', function() {
+            var v = new Vector2D(new Point2D(0,0),new Point2D(10,10));
+            v.set_p1(new Point2D(2,2))
+            assert_vactor_values(v, 2,2, 10,10, 8,8, Math.sqrt(8*8 + 8*8) );
+        })
+    })
+    describe('#set_p2', function() {
+        it('should set the values point 2', function() {
+            var v = new Vector2D(new Point2D(0,0),new Point2D(10,10));
+            v.set_p2(new Point2D(8,8))
+            assert_vactor_values(v, 0,0, 8,8, 8,8, Math.sqrt(8*8 + 8*8) );
+        })
+    })
+    describe('#translate', function() {
+        it('should translate both point of the vector by the specified values', function() {
+            var v = new Vector2D(new Point2D(0,0),new Point2D(10,10));
+            v.translate(10,10)
+            assert_vactor_values(v, 10,10, 20,20, 10,10, Math.sqrt(10*10 + 10*10) );
+        })
+    })
+    describe('#translate_p1', function() {
+        it('should translate point 1 of the vector by the specified values', function() {
+            var v = new Vector2D(new Point2D(0,0),new Point2D(10,10));
+            v.translate_p1(5,5)
+            assert_vactor_values(v, 5,5, 10,10, 5,5, Math.sqrt(5*5 + 5*5) );
+        })
+    })
+    describe('#translate_p2', function() {
+        it('should translate point 2 of the vector by the specified values', function() {
+            var v = new Vector2D(new Point2D(0,0),new Point2D(10,10));
+            v.translate_p2(5,5)
+            assert_vactor_values(v, 0,0, 15,15, 15,15, Math.sqrt(15*15 + 15*15) );
         })
     })
 })
--- utils/vector.js
+++ utils/vector.js
@@ -1,36 +1,46 @@
-var Vector2D = function(x1,y1,x2,y2) {
-    this.set(x1,y1,x2,y2);
-    
-    return this;
+var Point2D = require('./point.js').Point2D; 
+
+var Vector2D = function(p1,p2) {
+    this.set(p1,p2);
 }
 
-Vector2D.prototype.set = function(x1,y1,x2,y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+Vector2D.prototype.set = function(p1,p2) {
+    this.p1 = p1;
+    this.p2 = p2;
     
     this._update_derived_values();
 }
 
-Vector2D.prototype.set_p1 = function(x1,y1) {
-    this.x1 = x1;
-    this.y1 = y1;
+Vector2D.prototype.set_p1 = function(p) {
+    this.p1 = p;    
     this._update_derived_values();
 }
 
-Vector2D.prototype.set_p2 = function(x2,y2) {
-    this.x2 = x2;
-    this.y2 = y2;
+Vector2D.prototype.set_p2 = function(p) {
+    this.p2 = p;
+    this._update_derived_values();
+}
+
+Vector2D.prototype.translate = function(dx,dy) {
+    this.p1.translate(dx,dy);
+    this.p2.translate(dx,dy);
+}
+
+Vector2D.prototype.translate_p1 = function(dx,dy) {
+    this.p1.translate(dx,dy);
+    this._update_derived_values();
+}
+
+Vector2D.prototype.translate_p2 = function(dx,dy) {
+    this.p2.translate(dx,dy);
     this._update_derived_values();
 }
 
 Vector2D.prototype._update_derived_values = function() {
-    this.dx = this.x2 - this.x1;
-    this.dy = this.y2 - this.y1;
+    this.dx = this.p2.x - this.p1.x;
+    this.dy = this.p2.y - this.p1.y;
     this.d = Math.sqrt(this.dx*this.dx + this.dy*this.dy);
 }
-
 
 if(typeof module != 'undefined') {
     module.exports.Vector2D = Vector2D;
