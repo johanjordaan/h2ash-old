c14031b6d6ae28056c2dc2b2b31852380f2b9114
Johan Jordaan <djjordaan@gmail.com>
2013-05-12 22:17:04 +0200

    Updated Point2D to have init and to have various constructors etc

--- test/point.tests.js
+++ test/point.tests.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var assert = require('assert')
 var should = require('chai').should();
 var expect = require('chai').expect;
@@ -12,22 +13,41 @@ var assert_point_values = function(point,x,y) {
 
 describe('Point2D', function() {
     describe('#constructor', function() {
-        it('should create a new point with all values set to the specified values', function() {
-            var p = new point.Point2D(10,10);
-            assert_point_values(p, 10, 10 );
-        })
-		it('should create a new point default values', function() {
+        it('should create a new point with no values assigned', function() {
             var p = new point.Point2D();
-            assert_point_values(p, 0, 0 );
+            assert.equal(typeof(p.x),'undefined');
+			assert.equal(typeof(p.y),'undefined');
         })
+		it('should create a new point with the value specified (cloned from a previous point)', function() {
+            var p = new point.Point2D();
+            p.set_c(10,10);
+			var p2 = new point.Point2D(p);
+			assert_point_values(p2, 10, 10 );
+        })
+		it('should create a new point with the value specified (cloned from a previous point)', function() {
+            var p = new point.Point2D();
+            p.set_c(10,10);
+			assert_point_values(p, 10, 10 );
+        })
+
     })
-    describe('#set', function() {
-        it('should set the points coordinates to the new values', function() {
-            var p = new point.Point2D(10,10);
-            p.set(20,20);
+    describe('#set_c', function() {
+        it('should set the component values of the point', function() {
+            var p = new point.Point2D();
+            p.set_c(20,20);
             assert_point_values(p, 20, 20 );
         })
     })
+    describe('#set', function() {
+        it('should set point to have the same values as the source point', function() {
+            var p = new point.Point2D();
+            p.set_c(20,20);
+			var p2 = new point.Point2D();
+			p2.set(p);
+            assert_point_values(p, 20, 20 );
+        })
+    })
+
     describe('#translate', function() {
         it('should translate the points coordinates by the given delta values', function() {
             var p = new point.Point2D(10,10);
--- utils/point.js
+++ utils/point.js
@@ -1,12 +1,18 @@
 var _ = require('underscore');
 
-var Point2D = function(x,y) {
-	if(_.isUndefined(x) && _.isUndefined(this.x)) { x = 0; }
-	if(_.isUndefined(y) && _.isUndefined(this.y)) { y = 0; }
-	
+var Point2D = function(a,b) {
 	this.handlers = { on_change : [] };
-    this.set(x,y);
+
+	if(arguments.length == 0) {
+	} else if(arguments.length==1){
+		this.set(a);
+	} else {
+		this.set_c(a,b);
+	}
 }
+Point2D.prototype.init = function() {
+}
+
 Point2D.prototype.bind = function(key,func) {
 	this.handlers[key].push(func);
 }
@@ -16,13 +22,20 @@ Point2D.prototype._call_handlers = function(key) {
 	});
 }
 
-Point2D.prototype.set = function(x,y) {
+Point2D.prototype.set_c = function(x,y) {
     this.x = x;
     this.y = y;
 	this._call_handlers('on_change');
 }
+Point2D.prototype.set = function(p) {
+    this.x = p.x;
+    this.y = p.y;
+	this._call_handlers('on_change');
+}
+
+
 Point2D.prototype.translate = function(dx,dy) {
-    this.set(this.x+dx, this.y+dy); 
+    this.set_c(this.x+dx, this.y+dy); 
 	this._call_handlers('on_change');
 }
 
