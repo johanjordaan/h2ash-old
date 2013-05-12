88426ed6cc3004100462dbc1a033b2f073d040f7
Johan Jordaan <djjordaan@gmail.com>
2013-05-13 00:12:29 +0200

    Vector has been refactored to haev default and ref and c constructors and setters

--- test/vector.tests.js
+++ test/vector.tests.js
@@ -17,47 +17,123 @@ var assert_vactor_values = function(vector,x1,y1,x2,y2,dx,dy,length) {
 
 describe('Vector2D', function() {
     describe('#constructor', function() {
-        it('should create a new vector with all values set to the specified values', function() {
+		it('should create a new vector with all values set to the default', function() {
+            var v = new Vector2D();
+			assert.equal(typeof(v.p1),'undefined');
+			assert.equal(typeof(v.p2),'undefined');
+			assert.equal(typeof(v.dx),'undefined');
+			assert.equal(typeof(v.dy),'undefined');
+			assert.equal(typeof(v.length),'undefined');
+        });
+        it('should create a new vector and use the component values to set the points', function() {
             var v = new Vector2D(10,10,18,18);
             assert_vactor_values(v, 10,10, 18,18, 8,8, Math.sqrt(8*8 + 8*8) );
         });
-		it('should create a new vector with all values set to the default', function() {
-            var v = new Vector2D();
-            assert_vactor_values(v, 0,0, 0,0, 0,0, Math.sqrt(0*0 + 0*0) );
+        it('should create a new vector and use points to set the values of the points points (do not set the vector poiunts to ref the setting points) ', function() {
+            var p1 = new Point2D(10,10)
+			var p2 = new Point2D(18,18)
+			var v = new Vector2D(p1,p2);
+			p1.set(0,0);
+			p2.set(0,0);
+            assert_vactor_values(v, 10,10, 18,18, 8,8, Math.sqrt(8*8 + 8*8) );
+        });
+        it('should create a new vector and use ref the points specified', function() {
+            var p1 = new Point2D(10,10)
+			var p2 = new Point2D(18,18)
+			var v = new Vector2D(p1,p2,true);
+			p1.set_c(0,0);
+			p2.set_c(10,10);
+            assert_vactor_values(v, 0,0, 10,10, 10,10, Math.sqrt(10*10 + 10*10) );
         });
     });
-    
-    describe('#set', function() {
+    describe('#set_c', function() {
         it('should set the values of of both points', function() {
-            var v = new Vector2D(2,2,10,10);
+            var v = new Vector2D(0,0,0,0);
+			v.set_c(2,2,10,10);
+            assert_vactor_values(v, 2,2, 10,10, 8,8, Math.sqrt(8*8 + 8*8) );
+        })
+    })
+    describe('#set_p1_c', function() {
+        it('should set the values of of both points', function() {
+            var v = new Vector2D(0,0,0,0);
+			v.set_p1_c(2,2);
+            assert_vactor_values(v, 2,2, 0,0, -2,-2, Math.sqrt(2*2 + 2*2) );
+			v.p1.set_c(3,3);
+            assert_vactor_values(v, 3,3, 0,0, -3,-3, Math.sqrt(3*3 + 3*3) );
+
+        })
+    })
+    describe('#set_p2_c', function() {
+        it('should set the values of of both points', function() {
+            var v = new Vector2D(0,0,0,0);
+			v.set_p2_c(2,2);
+            assert_vactor_values(v, 0,0, 2,2, 2,2, Math.sqrt(2*2 + 2*2) );
+			v.p2.set_c(3,3);
+            assert_vactor_values(v, 0,0, 3,3, 3,3, Math.sqrt(3*3 + 3*3) );
+        })
+    })
+
+    describe('#set', function() {
+        it('should set the values of of both points using the source pointss data (dont ref the source points)', function() {
+            var v = new Vector2D(0,0,0,0);
+			var p1 = new Point2D(2,2);
+			var p2 = new Point2D(10,10);
+			v.set(p1,p2);
+			p1.set_c(0,0);
+			p2.set_c(0,0);
             assert_vactor_values(v, 2,2, 10,10, 8,8, Math.sqrt(8*8 + 8*8) );
         })
     })
     describe('#set_p1', function() {
-        it('should set the values point 1 (via vector)', function() {
+        it('should set the value p1 using the source point data (dont ref the source point)', function() {
             var v = new Vector2D(0,0,0,0);
-			v.set_p1(2,2);
+			var p1 = new Point2D(2,2);
+			v.set_p1(p1);
+			p1.set_c(0,0);
             assert_vactor_values(v, 2,2, 0,0, -2,-2, Math.sqrt(2*2 + 2*2) );
         })
-        it('should set the values point 1 (via point)', function() {
-            var v = new Vector2D(0,0,0,0);
-			v.p1.set(2,2);
-            assert_vactor_values(v, 2,2, 0,0, -2,-2, Math.sqrt(2*2 + 2*2) );
-        })
-
     })
     describe('#set_p2', function() {
-        it('should set the values point 2 (via vector)', function() {
+        it('should set the value p1 using the source point data (dont ref the source point)', function() {
             var v = new Vector2D(0,0,0,0);
-			v.set_p2(8,8);
-            assert_vactor_values(v, 0,0, 8,8, 8,8, Math.sqrt(8*8 + 8*8) );
-        })
-        it('should set the values point 2 (via point)', function() {
-            var v = new Vector2D(0,0,0,0);
-			v.p2.set(8,8);
-            assert_vactor_values(v, 0,0, 8,8, 8,8, Math.sqrt(8*8 + 8*8) );
+			var p2 = new Point2D(10,10);
+			v.set_p2(p2);
+			p2.set_c(0,0);
+            assert_vactor_values(v, 0,0, 10,10, 10,10, Math.sqrt(10*10 + 10*10) );
         })
     })
+
+
+    describe('#set_r', function() {
+        it('should set the vector points to the source points (ref the source points)', function() {
+            var v = new Vector2D(0,0,0,0);
+			var p1 = new Point2D(2,2);
+			var p2 = new Point2D(10,10);
+			v.set_r(p1,p2);
+			p1.set_c(8,8);
+			p2.set_c(0,0);
+            assert_vactor_values(v, 8,8, 0,0, -8,-8, Math.sqrt(8*8 + 8*8) );
+        })
+    })
+    describe('#set_p1_r', function() {
+        it('should set the vector p1 to the source point (ref the source point)', function() {
+            var v = new Vector2D(0,0,0,0);
+			var p1 = new Point2D(2,2);
+			v.set_p1_r(p1);
+			p1.set_c(8,8);
+            assert_vactor_values(v, 8,8, 0,0, -8,-8, Math.sqrt(8*8 + 8*8) );
+        })
+    })
+    describe('#set_p2_r', function() {
+        it('should set the vector p2 to the source point (ref the source point)', function() {
+            var v = new Vector2D(0,0,0,0);
+			var p2 = new Point2D(2,2);
+			v.set_p2_r(p2);
+			p2.set_c(9,2);
+            assert_vactor_values(v, 0,0, 9,2, 9,2, Math.sqrt(9*9 + 2*2) );
+        })
+    })
+	
     describe('#translate', function() {
         it('should translate both point of the vector by the specified values', function() {
             var v = new Vector2D(0,0,10,10);
@@ -66,29 +142,22 @@ describe('Vector2D', function() {
         })
     })
     describe('#translate_p1', function() {
-        it('should translate point 1 of the vector by the specified values (via vector))', function() {
+        it('should translate point 1 of the vector by the specified values (via vector and point)', function() {
             var v = new Vector2D(0,0,10,10);
             v.translate_p1(5,5)
             assert_vactor_values(v, 5,5, 10,10, 5,5, Math.sqrt(5*5 + 5*5) );
-        })
-        it('should translate point 1 of the vector by the specified values (via point)', function() {
-            var v = new Vector2D(0,0,10,10);
-            v.p1.translate(5,5)
-            assert_vactor_values(v, 5,5, 10,10, 5,5, Math.sqrt(5*5 + 5*5) );
+            v.p1.translate(10,10)
+            assert_vactor_values(v, 15,15, 10,10, -5,-5, Math.sqrt(5*5 + 5*5) );
         })
 
     })
     describe('#translate_p2', function() {
-        it('should translate point 2 of the vector by the specified values (via vector)', function() {
+        it('should translate point 2 of the vector by the specified values (via vector and point)', function() {
             var v = new Vector2D(0,0,10,10);
             v.translate_p2(5,5)
             assert_vactor_values(v, 0,0, 15,15, 15,15, Math.sqrt(15*15 + 15*15) );
+            v.p2.translate(-10,-10)
+            assert_vactor_values(v, 0,0, 5,5, 5,5, Math.sqrt(5*5 + 5*5) );
         })
-        it('should translate point 2 of the vector by the specified values (via point)', function() {
-            var v = new Vector2D(0,0,10,10);
-            v.p2.translate(5,5)
-            assert_vactor_values(v, 0,0, 15,15, 15,15, Math.sqrt(15*15 + 15*15) );
-        })
-
     })
 })
--- utils/vector.js
+++ utils/vector.js
@@ -1,33 +1,77 @@
 var _ = require('underscore');
 var Point2D = require('./point.js').Point2D; 
 
-var Vector2D = function(x1,y1,x2,y2) {
-	var that = this;
+var Vector2D = function(a,b,c,d) {
 	
-	if(_.isUndefined(x1) && _.isUndefined(y1)) { x1 = 0; y1 = 0 };
-	if(_.isUndefined(x2) && _.isUndefined(y2)) { x2 = 0; y2 = 0 };
+	if(arguments.length == 0) {					// Default empty constructor
+	} else if(arguments.length == 2) {			// Create copy of two points
+		this.p1 = new Point2D(a.x,a.y);
+		this.p2 = new Point2D(b.x,b.y);
+		this.init();
+	} else if(arguments.length == 3) {			// Create a ref of the points (add a thirt component as true to get a ref on construction)
+		if(c==true) {
+			this.set_r(a,b);
+		} 
+	}else {										// Create bthe points from components
+		this.p1 = new Point2D(a,b);
+		this.p2 = new Point2D(c,d);
+		this.init();
+	}
+}	
 
-	if(_.isUndefined(this.p1)) { this.p1 = new Point2D(x1,y1); } else { this.p1.set(x1,y1); }
-	if(_.isUndefined(this.p2)) { this.p2 = new Point2D(x2,y2); } else { this.p2.set(x2,y2); }
-	
+Vector2D.prototype.init = function() {
+	var that = this;
+
 	this.p1.bind('on_change',function() { that._update_derived_values() });
 	this.p2.bind('on_change',function() { that._update_derived_values() });
 	
 	that._update_derived_values();
 }
 
-Vector2D.prototype.set = function(x1,y1,x2,y2) {
-    this.p1.set(x1,y1);
-	this.p2.set(x2,y2);
+// Ref set
+//
+Vector2D.prototype.set_r = function(p1,p2) {
+    this.p1 = p1;
+	this.p2 = p2;
+	this.init();
+}
+Vector2D.prototype.set_p1_r = function(p1) {
+    this.p1 = p1;
+	this.init();
+}
+Vector2D.prototype.set_p2_r = function(p2) {
+    this.p2 = p2;
+	this.init();
 }
 
-Vector2D.prototype.set_p1 = function(x1,y1) {
-    this.p1.set(x1,y1);
+
+// Set the components of the existing objects
+//
+Vector2D.prototype.set_c = function(x1,y1,x2,y2) {
+    this.p1.set_c(x1,y1);
+	this.p2.set_c(x2,y2);
+}
+Vector2D.prototype.set_p1_c = function(x1,y1) {
+    this.p1.set_c(x1,y1);
+}
+Vector2D.prototype.set_p2_c = function(x2,y2) {
+	this.p2.set_c(x2,y2);
 }
 
-Vector2D.prototype.set_p2 = function(x2,y2) {
-    this.p2.set(x2,y2);
+// Default set from domain
+//
+Vector2D.prototype.set = function(p1,p2) {
+    this.p1.set(p1);
+	this.p2.set(p2);
 }
+Vector2D.prototype.set_p1 = function(p1) {
+    this.p1.set(p1);
+}
+Vector2D.prototype.set_p2 = function(p2) {
+    this.p2.set(p2);
+}
+
+
 
 Vector2D.prototype.translate = function(dx,dy) {
     this.p1.translate(dx,dy);
