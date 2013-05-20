391bb5d381adff7cb5dadff8ab64ac971a2622c5
Johan Jordaan <djjordaan@gmail.com>
2013-05-20 22:35:05 +0200

    Added the trig helper utils module

---
+++ test/trig.tests.js
@@ -0,0 +1,69 @@
+var _ = require('underscore');
+var assert = require('assert')
+var should = require('chai').should();
+var expect = require('chai').expect;
+
+var trig = require('../utils/trig.js');
+
+var float_error = 0.000001;
+
+describe('deg2rad',function(){
+	it('should convert degrees to radians - standard cases',function() {
+		var rad = trig.deg2rad(0);
+		rad.should.equal(0);
+		rad = trig.deg2rad(90);
+		rad.should.equal(Math.PI/2);
+		rad = trig.deg2rad(180);
+		rad.should.equal(Math.PI);
+		rad = trig.deg2rad(270);
+		rad.should.equal(Math.PI*3/2);
+		rad = trig.deg2rad(360);
+		rad.should.equal(Math.PI*2);
+	});
+});
+
+describe('rad2deg',function(){
+	it('should convert radians to degrees - standard cases',function() {
+		var deg = trig.rad2deg(0);
+		deg.should.equal(0);
+		deg = trig.rad2deg(Math.PI/2);
+		deg.should.equal(90);
+		deg = trig.rad2deg(Math.PI);
+		deg.should.equal(180);
+		deg = trig.rad2deg(Math.PI*3/2);
+		deg.should.equal(270);
+		deg = trig.rad2deg(Math.PI*2);
+		deg.should.equal(360);
+	});
+});
+
+
+describe('min_angle_between',function(){
+	it('should calculate the minimum angle between the two given angles (general cases)',function(){
+		var min_angle = trig.min_angle_between(trig.deg2rad(350),trig.deg2rad(10));
+		trig.rad2deg(min_angle).should.be.within(20-float_error,20+float_error);
+		min_angle = trig.min_angle_between(trig.deg2rad(80),trig.deg2rad(100));
+		trig.rad2deg(min_angle).should.be.within(20-float_error,20+float_error);
+		min_angle = trig.min_angle_between(trig.deg2rad(10),trig.deg2rad(350));
+		trig.rad2deg(min_angle).should.be.within(-20-float_error,-20+float_error);
+		min_angle = trig.min_angle_between(trig.deg2rad(100),trig.deg2rad(80));
+		trig.rad2deg(min_angle).should.be.within(-20-float_error,-20+float_error);
+		min_angle = trig.min_angle_between(trig.deg2rad(169),trig.deg2rad(350));
+		trig.rad2deg(min_angle).should.be.within(-179-float_error,-179+float_error);
+		min_angle = trig.min_angle_between(trig.deg2rad(171),trig.deg2rad(350));
+		trig.rad2deg(min_angle).should.be.within(179-float_error,179+float_error);
+	});
+	it('should calculate the minimum angle between the two given angles - 0 cases',function(){
+		var min_angle = trig.min_angle_between(trig.deg2rad(0),trig.deg2rad(0));
+		trig.rad2deg(min_angle).should.be.within(0-float_error,0+float_error);
+		min_angle = trig.min_angle_between(trig.deg2rad(360),trig.deg2rad(0));
+		trig.rad2deg(min_angle).should.be.within(0-float_error,0+float_error);
+		min_angle = trig.min_angle_between(trig.deg2rad(0),trig.deg2rad(360));
+		trig.rad2deg(min_angle).should.be.within(0-float_error,0+float_error);
+		
+		// The below case fails but that is ok. I dont want to introduce the overhead 
+		// of handling it Maybee handling it in the conversion is the answer 
+		//min_angle = trig.min_angle_between(trig.deg2rad(0),trig.deg2rad(720));
+		//trig.rad2deg(min_angle).should.be.within(0-float_error,0+float_error);
+	});
+});
---
+++ utils/trig.js
@@ -0,0 +1,35 @@
+if(typeof(require) == 'undefined') {
+} else {
+	_ = require('underscore');
+}
+
+var deg2rad = function(deg) { return (deg*(Math.PI/180.)); }
+var rad2deg = function(rad) { return rad*(180./Math.PI); }
+
+
+// theta in radians relative to x axis - counter clockwise
+// r is always > 0
+
+var c2p = function(x,y) {
+	return [Math.sqrt(x*x + y*y),Math.atan2(y,x)];	// [r,theta]
+} 
+
+var p2c = function(r,theta) {
+	return [r*Math.cos(theta),r*Math.sin(theta)]; 	// [x,y]
+}
+
+var min_angle_between = function(theta1,theta2) {
+	var delta = theta2 - theta1;
+	if(delta>Math.PI) delta = delta -2*Math.PI;
+	if(delta<-Math.PI) delta = 2*Math.PI +delta;
+	return delta;
+} 
+
+if(typeof module != 'undefined') {
+    module.exports.deg2rad = deg2rad;
+	module.exports.rad2deg = rad2deg;
+	module.exports.c2p = c2p;
+	module.exports.p2c = p2c;
+	module.exports.min_angle_between = min_angle_between;
+} else {
+}
