77c0e6131aaf6e31a34161df683e0a38d12c65b5
Johan Jordaan <djjordaan@gmail.com>
2013-06-02 07:36:15 +0200

    Added the grid fx module and its tests

---
+++ fx/grid_fx.js
@@ -0,0 +1,37 @@
+if(typeof(require) == 'undefined') {
+} else {
+	_ = require('underscore');
+}
+
+var GridFX = function(scene,color,step) {
+	this.scene = scene;
+
+	this.color = color;
+	this.step = step; 
+}
+
+GridFX.prototype.render = function(timestamp) {
+	var x_offset = this.scene.camera.center_x*this.scene.camera.magnification - this.scene.screen.width/2;
+	var y_offset = this.scene.camera.center_y*this.scene.camera.magnification - this.scene.screen.height/2;
+
+	this.scene.screen.context.save();
+	this.scene.screen.context.strokeStyle = this.color;
+	
+	for(var r=0;r<this.scene.screen.height;r+=this.step) {
+		this.scene.screen.context.moveTo(0,r+y_offset);
+		this.scene.screen.context.lineTo(this.scene.screen.width,r+y_offset);
+	}
+	
+	for(var c=0;c<this.scene.screen.width;c+=this.step) {
+		this.scene.screen.context.moveTo(c+x_offset,0);
+		this.scene.screen.context.lineTo(c+x_offset,this.scene.screen.height);
+	}
+
+	this.scene.screen.context.stroke();
+	this.scene.screen.context.restore();
+}
+
+if(typeof module != 'undefined') {
+	module.exports.GridFX = GridFX;
+} else {
+}
---
+++ test/grid_fx.tests.js
@@ -0,0 +1,53 @@
+var _ = require('underscore');
+var assert = require('assert');
+var should = require('chai').should();
+var expect = require('chai').expect;
+
+var Mock = require('../utils/mock.js').Mock;
+var printf = require('../utils/printf.js').printf
+
+var Camera = require('../utils/camera.js').Camera;
+var Screen = require('../utils/screen.js').Screen;
+var Scene = require('../utils/scene.js').Scene;
+
+var GridFX = require('../fx/grid_fx.js').GridFX;
+
+describe('GridFX',function() {
+	describe('#render',function() {
+		it('should should render the grid fx correctly at the specified location',function() { 
+			var context = new Mock(['save','restore','beginPath','moveTo','lineTo']);
+			
+			var screen = new Screen(640,480,context);
+			var camera = new Camera(14960000,0,0.007848062);
+			var scene = new Scene(screen,camera);
+			
+			var grid = new GridFX(scene,'gray',40);
+			
+//			var x_offset = this.scene.camera.center_x*this.scene.camera.magnification - this.scene.screen.width/2;
+//			var y_offset = this.scene.camera.center_y*this.scene.camera.magnification - this.scene.screen.height/2;
+			
+			
+			context.expect('stroke',[],function(){
+				context.strokeStyle.should.equal('gray');
+			});
+			
+			/*context.expect('arc',[0,0,50,0,2*Math.PI,false]);
+			var lable_xy = Math.sqrt((50*50)/2);
+			context.expect('moveTo',[lable_xy+2,lable_xy+2]);
+			context.expect('lineTo',[56,56]);
+			context.expect('fillText',['earth',56,51]);
+			context.expect('measureText',['earth'],function(text){
+				return({width:10});
+			});
+			context.expect('lineTo',[66,56]);
+*/
+			
+			grid.render()
+
+			context.validate(true).status.should.equal('ok');
+		});
+		
+		// Need a test for offset drawing
+	});
+});
+
