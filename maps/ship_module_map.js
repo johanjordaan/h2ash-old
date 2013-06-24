63ff934dfdf82dc202df9cc3f609e8d74253b7c9
Johan Jordaan <djjordaan@gmail.com>
2013-06-24 13:55:16 +0200

    Added the ship_module_map

---
+++ maps/ship_module_map.js
@@ -0,0 +1,20 @@
+if(typeof(require) == 'undefined') {
+} else {
+	_ = require('underscore');
+	ShipModule = require('../models/ship_module.js').ShipModule;
+}
+var ship_module_map = {
+	model_name	: 'ShipModule',
+	fields		: {
+		base_latency 	: { type:'Simple', default_value:0 },
+		activated		: { type:'Simple', default_value:false,conversion:function(val){ return val=='true'} },
+		completion_time	: { type:'Simple', default_value:0/*, conversion:time.get_timestamp*/ },
+	},
+	default_collection	: 'ShipModules',
+	cls : ShipModule
+}
+
+if(typeof module != 'undefined') {
+    module.exports.ship_module_map = ship_module_map;
+} else {
+}
